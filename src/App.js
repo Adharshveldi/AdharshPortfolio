9afa2dc68dfdceded6a23d58af13f862007aa663
Adharshveldi <adharsh083@gmail.com>
2025-03-19 20:28:00 -0500

    10

--- src/App.js
+++ src/App.js
@@ -17,33 +17,56 @@ import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// function App() {
+//   const [load, upadateLoad] = useState(true);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       upadateLoad(false);
+//     }, 1200);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <Router>
+//       <Preloader load={load} />
+//       <div className="App" id={load ? "no-scroll" : "scroll"}>
+//         <Navbar />
+//         <ScrollToTop />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/project" element={<Projects />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/resume" element={<Resume />} />
+//           <Route path="*" element={<Navigate to="/"/>} />
+//         </Routes>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 function App() {
-  const [load, upadateLoad] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <Router>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
-        <Navbar />
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="*" element={<Navigate to="/"/>} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
